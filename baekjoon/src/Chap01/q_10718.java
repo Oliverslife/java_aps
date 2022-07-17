9a6d30594222145c08311cb53169b03ba5941602
Sojin <voise.ksing@gmail.com>
2022-07-18 00:28:56 +0900

    22.07.18

---
+++ baekjoon/src/Chap01/q_10718.java
@@ -0,0 +1,9 @@
+package Chap01;
+
+public class q_10718 {
+    public static void main(String[] args) {
+        for(int i=0; i<2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
--- baekjoon/src/Chap01/q_2557.java
+++ baekjoon/src/Chap01/q_2557.java
@@ -1,4 +1,8 @@
 package Chap01;
 
 public class q_2557 {
+    public static void main(String[] args) {
+
+        System.out.print("Hello World!");
+    }
 }
