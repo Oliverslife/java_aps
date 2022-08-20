759b626eec37baa5bcaf0df3183fe447762d43ed
Sojin <voise.ksing@gmail.com>
2022-08-21 01:10:29 +0900

    22.08.21

---
+++ baekjoon/src/SSAFY_IM준비/q10158.java
@@ -0,0 +1,30 @@
+package SSAFY_IM준비;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class q10158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+
+        p += t % (w * 2);
+        if(p > w)
+            p = Math.abs(w * 2 - p);
+        q += t % (h * 2);
+        if(q > h)
+            q = Math.abs(h * 2 - q);
+
+        bw.write(p + " " + q);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
