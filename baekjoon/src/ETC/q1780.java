85571546ee9c8e4f6c1496b1fc2fc07ecfddcd44
Sojin <voise.ksing@gmail.com>
2022-10-25 00:02:03 +0900

    22.10.25

---
+++ baekjoon/src/ETC/q1780.java
@@ -0,0 +1,10 @@
+package ETC;
+
+import java.io.*;
+
+public class q1780 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
