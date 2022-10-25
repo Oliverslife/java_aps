c5e965586c293f2d756f20e761b1e97717924dbd
Sojin <voise.ksing@gmail.com>
2022-10-26 01:00:01 +0900

    22.10.26

--- baekjoon/src/ETC/q1780_solving.java
+++ baekjoon/src/ETC/q1780_solving.java
@@ -1,10 +1,16 @@
 package ETC;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
-public class q1780 {
+public class q1780_solving {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] paper = new int[n][n];
+
     }
 }
