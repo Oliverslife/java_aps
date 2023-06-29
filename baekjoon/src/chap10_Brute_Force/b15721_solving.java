888941b422a16008a3ce9e037225bb61b2f60125
MyFinale <lunar7s@naver.com>
2023-06-30 01:15:00 +0900

    23.06.30

---
+++ baekjoon/src/chap10_Brute_Force/b15721_solving.java
@@ -0,0 +1,19 @@
+package chap10_Brute_Force;
+
+import java.util.Scanner;
+
+public class b15721_solving {
+
+    public static void main(String[] args) {
+
+        int A, T, GUHO, round = 0, unit = 4;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        T = sc.nextInt();
+        GUHO = sc.nextInt();
+        sc.close();
+
+
+    }
+}
