291ccf675fac0f5b08f545b87360a4ab7d3a9701
tail-s <lunar7s@naver.com>
2023-08-09 01:48:41 +0900

    23.08.09

---
+++ baekjoon/src/Chap07_Two_Pointer/b11728_pq.java
@@ -0,0 +1,39 @@
+package Chap07_Two_Pointer;
+
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class b11728_pq {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> pq = new PriorityQueue<>();
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens())
+            pq.offer(Integer.parseInt(st.nextToken()));
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens())
+            pq.offer(Integer.parseInt(st.nextToken()));
+
+        while (!pq.isEmpty())
+            sb.append(pq.poll()).append(" ");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
---
+++ baekjoon/src/Chap07_Two_Pointer/b11728_tp.java
@@ -0,0 +1,43 @@
+package Chap07_Two_Pointer;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class b11728_tp {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        int[] B = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++)
+            A[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<M; i++)
+            B[i] = Integer.parseInt(st.nextToken());
+
+        int i = 0, j = 0;
+        while (i < N && j < M)
+            sb.append(A[i] < B[j] ? A[i++] : B[j++]).append(" ");
+        while (i < N)
+            sb.append(A[i++]).append(" ");
+        while (j < M)
+            sb.append(B[j++]).append(" ");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
