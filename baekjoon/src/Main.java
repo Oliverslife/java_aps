e4a96335d8f5cdfbe028b983e9d36f4c888d61e5
tail-s <lunar7s@naver.com>
2023-08-14 23:52:37 +0900

    Now Solving...

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,13 +1,53 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * solving...
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        sc.close();
+    static int N, K;
+    static int[] S;
 
-        System.out.println(A > B ? ">" : A < B ? "<" : "==");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        S = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<N; i++)
+            S[i] = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+        int start = 0, end = 0, delete = 0, len = 0, ans = 0;
+        while (end < N) {
+
+            while (end < N && delete < K) {
+                if (S[end++] % 2 != 0)
+                    delete++;
+                else
+                    len++;
+            }
+
+            while (end < N - 1 && S[end + 1] % 2 == 0) {
+                end++;
+                len++;
+            }
+
+            ans = Math.max(ans, len);
+            if (S[start++] % 2 != 0)
+                delete--;
+            else
+                len--;
+        }
+
+        System.out.println(len);
     }
 }
