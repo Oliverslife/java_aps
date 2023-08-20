de6a766ed10f112f9dd89ed97a81b608cd84986a
tail-s <lunar7s@naver.com>
2023-08-21 03:18:35 +0900

    solved

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * solving...
+ * sliding window
  */
 public class Main {
 
@@ -12,12 +12,35 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = stoi(st.nextToken());
-        int d = stoi(st.nextToken());
-        int k = stoi(st.nextToken());
-        int c = stoi(st.nextToken());
+        int N, d, k, c, tmp = 0, ans = 0;
+        int[] arr, selected;
 
-        bw.write();
+        N = stoi(st.nextToken());   // 회전 초밥 벨트에 놓은 접시의 수
+        d = stoi(st.nextToken());   // 초밥의 가짓수
+        k = stoi(st.nextToken());   // 연속해서 먹는 접시의 수
+        c = stoi(st.nextToken());   // 쿠폰 번호
+
+        arr = new int[N];
+        selected = new int[d + 1];
+
+        for (int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<k; i++) {
+            if (selected[arr[i]] == 0) tmp++;
+            selected[arr[i]]++;
+        }
+        ans = selected[c] == 0 ? tmp + 1 : tmp;
+
+        for (int i=0; i<N; i++) {
+            if (selected[arr[(i + k) % N]] == 0) tmp++;
+            selected[arr[(i + k) % N]]++;
+            selected[arr[i]]--;
+            if (selected[arr[i]] == 0) tmp--;
+            ans = Math.max(ans, selected[c] == 0 ? tmp + 1 : tmp);
+        }
+
+        bw.write(ans + "");
 
         bw.flush();
         bw.close();
